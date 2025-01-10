9a99573b64d263dc7a7d067572ac0b3e4cde81f5
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-01-11 00:43:22 +0200

    Updated the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,9 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-
+import Hero from "./Hero/Hero";
 function App() {
-  const [result, setResult] = useState(0);
-  const [min, setMin] = useState(1);
-  const [max, setMax] = useState(10);
-  const [prev, setPrev] = useState(0);
-  const [numbers, setNumbers] = useState([]);
-
-  const handleRandomNumber = (min, max) => {
-    if (max > 100) {
-      return;
-    }
-    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    if (randomNumber === prev) {
-      handleRandomNumber(min, max);
-      return;
-    }
-    setPrev(randomNumber);
-    setResult(randomNumber);
-  };
-  useEffect(() => {
-    let newNumbers = [];
-    for (let i = min; i <= max; i++) {
-      newNumbers = [...newNumbers, i];
-    }
-    if (max > 100) {
-      setNumbers([]);
-    } else {
-      setNumbers(newNumbers);
-    }
-  }, [min, max]);
-  console.log(numbers);
   return (
     <>
-      <div className="App">
-        <div className={"maincontainer"}>
-          <div>{result}</div>
-          <input
-            type="number"
-            onChange={(e) => {
-              setMin(Number(e.target.value));
-            }}
-          />
-          <input
-            type="number"
-            onChange={(e) => setMax(Number(e.target.value))}
-          />
-          <button onClick={() => handleRandomNumber(min, max)}>Generate</button>
-        </div>
-
-        <div className="tablecontainer">
-          <div className="numberscontainer">
-            {numbers.map((number, index) => (
-              <div className="number" key={index}>
-                {number}
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <Hero />
     </>
   );
 }
---
+++ src/Hero/Hero.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import "./Hero.css";
+
+function Hero() {
+  const [result, setResult] = useState(0);
+  const [min, setMin] = useState(1);
+  const [max, setMax] = useState(5);
+  const [prev, setPrev] = useState(0);
+  const [prevIndex, setPrevIndex] = useState(null); // Store the previous index
+  const [numbers, setNumbers] = useState([]);
+  const [isCycling, setIsCycling] = useState(false);
+
+  const handleRandomNumber = (min, max) => {
+    if (max > 100) {
+      return;
+    }
+    let randomNumber;
+    do {
+      randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    } while (randomNumber === prev);
+
+    setPrev(randomNumber);
+    setResult(randomNumber);
+  };
+
+  useEffect(() => {
+    let newNumbers = [];
+    for (let i = min; i <= max; i++) {
+      newNumbers = [...newNumbers, i];
+    }
+    if (max > 100) {
+      setNumbers([]);
+    } else {
+      setNumbers(newNumbers);
+    }
+  }, [min, max]);
+
+  useEffect(() => {
+    if (isCycling) {
+      const intervalId = setInterval(() => {
+        let randomIndex;
+        do {
+          randomIndex = Math.floor(Math.random() * numbers.length);
+        } while (randomIndex === prevIndex);
+
+        setPrevIndex(randomIndex);
+        setResult(numbers[randomIndex]);
+      }, 300);
+
+      return () => clearInterval(intervalId);
+    }
+  }, [isCycling, numbers, prevIndex]);
+
+  const handleButtonClick = () => {
+    setIsCycling(true);
+    setTimeout(() => {
+      setIsCycling(false);
+    }, 3900);
+    setTimeout(() => {
+      handleRandomNumber(min, max);
+    }, 4400);
+    setTimeout(() => {
+      handleRandomNumber(min, max);
+    }, 5200);
+  };
+
+  return (
+    <div className="hero">
+      <div className="maincontainer">
+        <div>{result}</div>
+        <input
+          type="number"
+          onChange={(e) => {
+            setMin(Number(e.target.value));
+          }}
+        />
+        <input type="number" onChange={(e) => setMax(Number(e.target.value))} />
+        <button onClick={handleButtonClick}>Generate</button>
+      </div>
+
+      <div className="tablecontainer">
+        <div className="numberscontainer">
+          {numbers.map((number, index) => (
+            <div
+              className={`number ${number === result ? "selected" : ""} ${
+                isCycling ? "cycling" : ""
+              }`}
+              key={index}
+            >
+              {number}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
